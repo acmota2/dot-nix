c1257a2f0ef02f066ee97cf83ea3e6c2f4f3cd63
acmota2 <acmota2@gmail.com>
2024-12-01 13:18:22 +0000

    Changed terminal to foot

--- default.nix
+++ default.nix
@@ -8,7 +8,7 @@
         ./base
         ./boot
         ./desktop/${desktop}
-        ./kitty
+        ./foot
         ./locale
         ./hardware
         ./machines/${hostname}.nix
--- foot/default.nix
+++ foot/default.nix
@@ -1,8 +1,8 @@
 { home-manager, username, pkgs, ...}:
 {
     home-manager.users.${username} = _: {
-    	home.file.".config/foot" = {
-	    source = "./.";
+	    home.file.".config/foot/foot.ini" = {
+	    source = ./foot.ini;
 	    recursive = true;
 	};
     	home.packages = with pkgs; [ foot ];
