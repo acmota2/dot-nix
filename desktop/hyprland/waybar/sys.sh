77148e4d321309ba1a54af978e549f150c7b72f5
acmota2 <acmota2@gmail.com>
2024-12-01 12:54:45 +0000

    Adding waybar and updating hyprland config

---
+++ desktop/hyprland/waybar/sys.sh
@@ -0,0 +1,5 @@
+hyprctl dispatch togglefloating > /dev/null
+hyprctl dispatch resizeactive exact 800 480 > /dev/null
+hyprctl dispatch centerwindow > /dev/null
+macchina
+read -p "Press Enter to close..."
