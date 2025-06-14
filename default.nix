f2a1b5c8c583375121a030c8bb07b0560df85e3d
acmota2 <acmota2@gmail.com>
2025-06-15 01:55:55 +0200

    feat: separated dev and lnguages

--- default.nix
+++ default.nix
@@ -1,35 +1,37 @@
-{ pkgs, home-manager, desktop, username, hostname, ... }:
+{ pkgs, home-manager, desktop, ... }:
 {
-    imports = [
-        home-manager.nixosModules.home-manager
-        ./base
-        ./boot
-        ./desktop/${desktop}
-	./foot
-        ./hardware
-        ./locale
-	./macchina
-	./machines
-	./mako
-  	./multimedia
-        ./nixvim
-	./starship
-        ./users
-        ./wofi
-    ];
+  imports = [
+    home-manager.nixosModules.home-manager
+    ./base
+    ./boot
+    ./desktop/${desktop}
+    ./dev
+    ./dev/languages.nix
+    ./foot
+    ./hardware
+    ./locale
+    ./macchina
+    ./machines
+    ./mako
+    ./multimedia
+    ./nixvim
+    ./starship
+    ./users
+    ./wofi
+  ];
 
-    environment.systemPackages = with pkgs; [
-        file
-        git
-        wget
-        curl
-        kitty
-        zsh
-        firefox
-        discord
-        coreutils-full
-        macchina
-	zoxide
-	starship
-    ];
+  environment.systemPackages = with pkgs; [
+    file
+    git
+    wget
+    curl
+    kitty
+    zsh
+    firefox
+    discord
+    coreutils-full
+    macchina
+    zoxide
+    starship
+  ];
 }
