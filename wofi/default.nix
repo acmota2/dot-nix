faf97cc92ff8d58d4950670f78e8250c98b7f1d8
acmota2 <acmota2@gmail.com>
2025-06-14 17:39:59 +0200

    Added correct wofi config

--- wofi/default.nix
+++ wofi/default.nix
@@ -1,10 +1,10 @@
 { home-manager, username, pkgs, ... }:
 {
     home-manager.users.${username} = _: {
-        home.file.".config/wofi" = {
-            source = ./.;
-            recursive = true;
+        home = {
+		file.".config/wofi/config".source = ./config;
+		file.".config/wofi/style.css".source = ./style.css;
+    		packages = with pkgs; [ wofi ];
         };
-    	home.packages = with pkgs; [ wofi ];
     };
 }
