cc75aefc5ce9f72f996724fa6815776db6538500
acmota2 <acmota2@gmail.com>
2025-06-15 00:59:09 +0200

    fix: hibernation

--- boot/default.nix
+++ boot/default.nix
@@ -1,13 +1,17 @@
-{ pkgs, ... }:
 {
-    boot = {
-        loader = {
-            systemd-boot.enable = true;
-            efi.canTouchEfiVariables = true;
-        };
-        plymouth = {
-            enable = true;
-            theme = "bgrt";
-        };
+  boot = {
+    loader = {
+      efi.canTouchEfiVariables = true;
+      grub = {
+        enable = true;
+        efiSupport = true;
+        device = "nodev";
+      };
     };
+    plymouth = {
+        enable = true;
+        theme = "bgrt";
+    };
+    resumeDevice = "/dev/disk/by-uuid/4d4efbda-9f85-42a0-adf0-4c60d9f6d6e6";
+  };
 }
