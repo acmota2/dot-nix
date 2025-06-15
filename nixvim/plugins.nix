ae02c8944ac509f99023ede6f438431a87d59eb3
acmota2 <acmota2@gmail.com>
2025-06-15 02:50:09 +0200

    feat(nixvim): corrected cmp config

--- nixvim/plugins.nix
+++ nixvim/plugins.nix
@@ -51,14 +51,23 @@
     };
     cmp = {
         enable = true;
-        settings.mapping = {
+        settings = {
+          mapping = {
             "<C-b>" = "cmp.mapping.scroll_docs(-4)";
             "<C-f>" = "cmp.mapping.scroll_docs(4)";
             "<C-Space>" = "cmp.mapping.complete()";
             "<C-e>" = "cmp.mapping.abort()";
             "<CR>" = "cmp.mapping.confirm({ select = true })";
+          };
+          autoEnableSources = true;
+          sources = [
+            { name = "nvim_lsp"; }
+            { name = "path"; }
+            { name = "buffer"; }
+          ];
         };
     };
+    markdown-preview.enable = true;
     treesitter.enable = true;
     web-devicons.enable = true;
 }
