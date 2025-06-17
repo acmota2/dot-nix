3867d9de8bc25efd5b47007f6326f9d76e223130
acmota2 <acmota2@gmail.com>
2025-06-17 19:41:31 +0200

    refactor(hyprland): changed the opacity of active terminal window

--- desktop/hyprland/rules.nix
+++ desktop/hyprland/rules.nix
@@ -1,6 +1,6 @@
 {
   wayland.windowManager.hyprland.settings.windowrulev2 = [
-    "opacity 0.8 0.6, class:^($terminalWindow)$"
+    "opacity 0.9 0.6, class:^($terminalWindow)$"
     "animation slide down, title:^($terminalWindow)$"
     "stayfocused, title:^()$,class:^(steam)$"
     "minsize 1 1, title:^()$,class:^(steam)$"
