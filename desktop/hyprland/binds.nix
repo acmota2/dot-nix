bf35ece7c642807f87618d3ded239ba38bcd3f21
acmota2 <acmota2@gmail.com>
2025-06-17 19:10:48 +0200

    fix(hyprland): mods

--- desktop/hyprland/binds.nix
+++ desktop/hyprland/binds.nix
@@ -1,25 +1,16 @@
 let
-  workspaceBinds =
-    [
-      "$mod, F, exec, firefox"
-      ", Print, exec, grimblast copy area"
-    ]
-    ++ (
-      # workspaces
-      # binds $mod + [shift +] {1..9} to [move to] workspace {1..9}
-      builtins.concatLists (
-        builtins.genList (
-          i:
-          let
-            ws = i + 1;
-          in
-          [
-            "$mod, code:1${toString i}, workspace, ${toString ws}"
-            "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
-          ]
-        ) 9
-      )
-    );
+  workspaceBinds = builtins.concatLists (
+    builtins.genList (
+      i:
+      let
+        ws = i + 1;
+      in
+      [
+        "$mod, code:1${toString i}, workspace, ${toString ws}"
+        "$mod SHIFT, code:1${toString i}, movetoworkspace, ${toString ws}"
+      ]
+    ) 9
+  );
 in
 {
   wayland.windowManager.hyprland.settings = {
