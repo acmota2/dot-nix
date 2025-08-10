25551475155010a446e8970123b1369d8989d936
acmota2 <acmota2@gmail.com>
2025-08-10 17:05:47 +0200

    feat(macchina): added new macchina theme for different host

--- macchina/default.nix
+++ macchina/default.nix
@@ -1,9 +1,14 @@
-{ pkgs, username, ... }: {
+{
+  pkgs,
+  username,
+  hostname,
+  ...
+}:
+{
   home-manager.users.${username} = _: {
-    home.file.".config/macchina" = {
-      source = ./config;
-      recursive = true;
-    };
+    home.file.".config/macchina/macchina.toml".source = ./config/macchina.toml;
+    home.file.".config/macchina/theme/main.toml".source = ./config/themes/main.toml;
+    home.file.".config/macchina/theme/art.txt".source = ./config/themes/${hostname}.txt;
     home.packages = with pkgs; [ macchina ];
   };
 }
