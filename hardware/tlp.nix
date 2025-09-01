423872283f41db28b50f9106858010f12e5d8b27
acmota2 <acmota2@gmail.com>
2025-09-01 20:01:31 +0200

    fix(tlp): scaling

--- hardware/tlp.nix
+++ hardware/tlp.nix
@@ -3,7 +3,7 @@
     enable = true;
     settings = {
       CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_SCALING_GOVERNOR_ON_BAT = "ondemand";
 
       CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
       CPU_ENERGY_PERF_POLICY_ON_BAT = "balance_power";
@@ -16,6 +16,8 @@
       #Optional helps save long term battery health
       START_CHARGE_THRESH_BAT0 = 40; # 40 and below it starts to charge
       STOP_CHARGE_THRESH_BAT0 = 80; # 80 and above it stops charging
+      START_CHARGE_THRESH_BAT1 = 40; # 40 and below it starts to charge
+      STOP_CHARGE_THRESH_BAT1 = 80; # 80 and above it stops charging
     };
   };
 }
