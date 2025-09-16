aa46d4602dc46bd91187bc6b41f81d6e708b9de1
acmota2 <acmota2@gmail.com>
2025-09-16 10:23:51 +0200

    feat(aws): added aws vault

--- aws/default.nix
+++ aws/default.nix
@@ -1,6 +1,23 @@
-{ pkgs, ... }:
+{ pkgs, username, ... }:
 {
-  environment.systemPackages = with pkgs; [
-    aws-vault
-  ];
+  home-manager.users."${username}" = _: {
+    home.sessionVariables = {
+      AWS_VAULT_BACKEND = "pass";
+      GPG_TTY = "$TTY";
+    };
+
+    home.packages = with pkgs; [
+      awscli2
+      aws-vault
+      pass
+    ];
+
+    programs.gpg.enable = true;
+
+    services.gpg-agent = {
+      enable = true;
+      enableSshSupport = true;
+      pinentry.package = pkgs.pinentry-curses;
+    };
+  };
 }
