d87ceff6cf8c67e3c7ff570ccc1cff933b19663a
acmota2 <acmota2@gmail.com>
2025-09-18 14:30:16 +0200

    fix(switch-script): now detects wsl correctly

--- switch.sh
+++ switch.sh
@@ -2,7 +2,7 @@
 
 HOSTNAME=$(cat /proc/sys/kernel/hostname)
 USERNAME=$(whoami)
-NIXOS=$(uname -v | grep NixOS)
+NIXOS=$(cat /etc/os-release | grep NixOS)
 IS_NIXOS=$?
 echo
 echo "Nix rebuild-switch helper"
@@ -11,8 +11,12 @@ if [ $IS_NIXOS -eq 0 ]; then
     read -r -p "I will ask to sudo now, do you want to proceed? [y/N] " response
     case "$response" in
         [yY][eE][sS]|[yY])
-            sudo cp /etc/nixos/hardware-configuration.nix ./machines/$HOSTNAME.nix
-            sudo nixos-rebuild switch --flake ~/repos/dot-nix#$HOSTNAME
+            WSL=$(uname -a | grep WSL)
+            IS_WSL=$?
+            if [[ $IS_WSL -ne 0 ]]; then
+                sudo cp /etc/nixos/hardware-configuration.nix ./machines/$HOSTNAME.nix
+            fi
+            sudo nixos-rebuild switch --flake .#$HOSTNAME
             ;;
         *)
             exit 1
