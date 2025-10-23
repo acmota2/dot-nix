095f21b4acc8103c0a5ce22f829ff2e6ea7430f4
acmota2 <acmota2@gmail.com>
2025-10-23 20:07:20 +0200

    feat(bash): changed to bash

--- shell/default.nix
+++ shell/default.nix
@@ -7,39 +7,39 @@
 myUtils.homeOrNixos {
   inherit isHomeManager username;
   options = {
-    programs.zsh = {
-      enable = true;
-      enableCompletion = true;
-
-      shellAliases = {
-        ls = "ls --color";
-        als = "ls -la";
-        cp = "cp -i";
-        edit = "sudo -e";
-        schlaf = "systemctl suspend";
-        ".." = "cd ..";
-        "..." = "cd ../..";
-        "...." = "cd ../../..";
+    programs = {
+      atuin.enableBashIntegration = true;
+      bash = {
+        enable = true;
+        enableCompletion = true;
+        shellAliases = {
+          ls = "ls --color";
+          als = "ls -la";
+          cp = "cp -i";
+          edit = "sudo -e";
+          schlaf = "systemctl suspend";
+          ".." = "cd ..";
+          "..." = "cd ../..";
+          "...." = "cd ../../..";
+        };
+        historySize = 1000;
       };
-
-      history = {
-        size = 1000;
-        path = "$HOME/.zsh_history";
+      readline = {
+        enable = true;
+        variables = {
+          bell-style = "none";
+        };
+        bindings = {
+          "\\e[H" = "beginning-of-line"; # Home key
+          "\\e[F" = "end-of-line"; # End key
+          "\\e[3~" = "delete-char"; # Delete key
+          "\\e[1;5C" = "forward-word"; # Ctrl+Right
+          "\\e[1;5D" = "backward-word"; # Ctrl+Left
+          "\\e\\C-?" = "backward-kill-word"; # Ctrl+Backspace
+          "\\e[3;5~" = "kill-word"; # Ctrl+Del
+          "\\e[Z" = "menu-complete-backward"; # Shift+Tab
+        };
       };
-      initContent = ''
-        zstyle ':completion:*' menu select
-
-        bindkey  "^[[H"   beginning-of-line
-        bindkey  "^[[F"   end-of-line
-        bindkey  "^[[3~"  delete-char
-        bindkey "^[[1;5C" forward-word
-        bindkey "^[[1;5D" backward-word
-        bindkey '^H' backward-kill-word
-        bindkey '5~' kill-word
-        bindkey '^[[Z' reverse-menu-complete
-
-        unsetopt BEEP
-      '';
     };
   };
 }
