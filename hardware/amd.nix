f2d2a91c4ca2b66e2256fe34e18a62187df83b65
acmota2 <acmota2@gmail.com>
2026-05-30 22:34:19 +0200

    refactor(amd): refactored amd gpu config

--- hardware/amd.nix
+++ hardware/amd.nix
@@ -1,9 +1,15 @@
-_: {
+{ monitors, ... }:
+{
   environment.variables.AMD_VULKAN_ICD = "RADV";
   hardware = {
+    amdgpu.overdrive.enable = true;
     graphics = {
       enable = true;
       enable32Bit = true;
     };
   };
+
+  boot.kernelParams = map (
+    m: "video=${m.output}:${toString m.width}x${toString m.height}MR@${toString m.refresh}Hz"
+  ) monitors;
 }
