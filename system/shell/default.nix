00abfa1d8caa8ab8ecc7eca475cbd466628de223
acmota2 <acmota2@gmail.com>
2026-06-17 02:12:49 +0200

    feat(fzf): added tokyonight color scheme

--- system/shell/default.nix
+++ system/shell/default.nix
@@ -18,7 +18,23 @@
         enable = true;
         nix-direnv.enable = true;
       };
-      fzf.enable = true;
+      fzf = {
+        enable = true;
+        colors = {
+          "bg+" = "#2f334d";
+          "bg" = "#24283b";
+          "spinner" = "#f7768e";
+          "hl" = "#7aa2f7";
+          "fg" = "#c0caf5";
+          "header" = "#7aa2f7";
+          "info" = "#bb9af7";
+          "pointer" = "#7dcfff";
+          "marker" = "#7dcfff";
+          "fg+" = "#c0caf5";
+          "prompt" = "#7aa2f7";
+          "hl+" = "#7dcfff";
+        };
+      };
       starship.enable = true;
       zoxide = {
         enable = true;
