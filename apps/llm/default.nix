895fdb5c73d874f1c28d8a2835118a16856caede
acmota2 <acmota2@gmail.com>
2026-09-05 19:10:31 +0200

    feat(llm): updated qwen

--- apps/llm/default.nix
+++ apps/llm/default.nix
@@ -50,8 +50,8 @@ in
             cmd = "${llama-pkg}/bin/llama-server --port \${PORT} -m /var/lib/llama-models/Qwen3.5-4B-Q8_0.gguf -ngl -1 -c 16384";
             ttl = 90;
           };
-          "qwen3.6-27b" = {
-            cmd = "${llama-pkg}/bin/llama-server --port \${PORT} -m /var/lib/llama-models/Qwen3.6-27B-IQ4_XS.gguf -ngl -1 -c 32768 --cache-ram 8192";
+          "qwen3.8-27b" = {
+            cmd = "${llama-pkg}/bin/llama-server --port \${PORT} -m /var/lib/llama-models/Qwen3.8-27B-UD-IQ4_XS.gguf -ngl -1 -c 49152 --cache-ram 8192";
             ttl = 10;
           };
           "qwen3.6-35b" = {
